39331d9ced7fff4fbe21b4952fc3fd5cbcab9c84
Ahmad Bagheri <u.ahmadbagheri@gmail.com>
2025-04-19 20:14:32 +0330

    [fix] search buttons
    convert normal search div to an input and advanced search to a button

--- src/assets/scripts/Background.tsx
+++ src/assets/scripts/Background.tsx
@@ -57,8 +57,16 @@ export default function BackGround() {
                     </div>
                     <div className='information'>
                         <div className="search">
-                            <div className='advanced-search'>جستجو پ</div>
-                            <div className='normal-search'>جستجو</div>
+                            <button className="advanced-search">جستجو پ</button> {/* Advanced search as a button */}
+                            <div className="normal-search">
+                                <input
+                                    type="text"
+                                    placeholder="جستجو"
+                                />
+                                <button className="search-button">
+                                    <i className="bi bi-search"></i>
+                                </button>
+                            </div>
                         </div>
                         <div className="charts">
                             <div className='chart-1'>نمودار</div>
