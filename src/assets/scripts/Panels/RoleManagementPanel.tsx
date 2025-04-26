42113dff73866343319934b9941016e5e83715b8
Ahmad Bagheri <u.ahmadbagheri@gmail.com>
2025-04-26 16:00:20 +0330

    [add] frames

--- src/assets/scripts/Panels/RoleManagementPanel.tsx
+++ src/assets/scripts/Panels/RoleManagementPanel.tsx
@@ -1,5 +1,26 @@
 export default function RoleManagementPanel() {
-    return (
-        <div style={{backgroundColor: 'black'}}>asdfasdf</div>
-    );
+  return (
+    <>
+      <div className="h-2/15 bg-black">as</div>
+      <div className="grid h-1/15 grid-cols-2 bg-red-600">
+        <div className="bg-yellow-300">nxc,vm</div>
+        <div className="bg-purple-500">asdas</div>
+      </div>
+      <div className="grid gap-5">
+        <div className="grid h-20 grid-cols-2 rounded-[25px] bg-white">
+          <div className="bg-red-500 rounded-[25px]"></div>
+          <div className="grid grid-cols-2 bg-orange-400 rounded-[25px]">
+            <div className="bg-black rounded-[25px]"></div>
+            <div className="bg-emerald-300 rounded-[25px]"></div>
+          </div>
+        </div>
+        <div className="h-20 rounded-[25px] bg-white"></div>
+        <div className="h-20 rounded-[25px] bg-white">asdfasdf</div>
+        <div className="h-20 rounded-[25px] bg-white">asdfasdf</div>
+        <div className="h-20 rounded-[25px] bg-white">asdfasdf</div>
+        <div className="h-20 rounded-[25px] bg-white">asdfasdf</div>
+        <div className="h-20 rounded-[25px] bg-white">asdfasdf</div>
+      </div>
+    </>
+  );
 }
