fe61f0d73d01095d0111a3b166751b8baf1e21c2
Amirmahdi-Eghbali <133925192+Amirmahdi-Eghbali@users.noreply.github.com>
2025-04-28 18:14:12 +0330

    ...

--- src/assets/scripts/main.tsx
+++ src/assets/scripts/main.tsx
@@ -2,12 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../styles/reset.scss'
 import DashboardComponent from './DashboardComponent'
-<<<<<<< HEAD
-import '../styles/Panels/MainDashboardPanel.scss';
-import 'bootstrap/dist/css/bootstrap.css'
-=======
 import { RTLProvider } from './RTLProvider'
->>>>>>> 611a7450cdae26bdf2c06c31988b31cca1063ed0
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
