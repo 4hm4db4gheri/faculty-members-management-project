8aab137b840ccbe598ec48d0d2c55c3045be0387
Amirmahdi-Eghbali <133925192+Amirmahdi-Eghbali@users.noreply.github.com>
2025-05-05 20:18:43 +0330

    [complete] login page front and its API

--- src/assets/scripts/main.tsx
+++ src/assets/scripts/main.tsx
@@ -1,13 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '../styles/reset.scss'
-import DashboardComponent from './DashboardComponent'
-import { RTLProvider } from './RTLProvider'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "../styles/main.css";
+import DashboardComponent from "./DashboardComponent";
+import SignUpPage from "./Panels/SignUpPage";
+import { RTLProvider } from "./RTLProvider";
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RTLProvider>
-      <DashboardComponent />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<DashboardComponent />} />
+        </Routes>
+      </BrowserRouter>
     </RTLProvider>
   </StrictMode>,
-)
+);
